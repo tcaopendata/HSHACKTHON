acce9dc32c81b21f9f509296ff63024f2a848eb7
Chiung Ta-Chiu <mentha5681@gmail.com>
2017-07-25 14:07:28 +0800

    Create title and '

--- 戀戀台三線網站/Controllers/HomeController.cs
+++ 戀戀台三線網站/Controllers/HomeController.cs
@@ -13,14 +13,14 @@ namespace 戀戀台三線網站.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Attractions()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Transportation()
         {
             ViewBag.Message = "Your contact page.";
 
