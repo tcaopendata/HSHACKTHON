4bbfb0f7e9c7aeb27cbc6820dab9f24b7d83ac05
Chiung Ta-Chiu <mentha5681@gmail.com>
2017-07-25 23:40:59 +0800

    完成路線1、2景點介紹

---
+++ 戀戀台三線網站/Controllers/PlanController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 戀戀台三線網站.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult plan1()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult plan2()
+        {
+            ViewBag.Message = "your contact page.";
+            return View();
+        }
+        public ActionResult plan3()
+        {
+            ViewBag.Message = "your contact page.";
+            return View();
+        }
+    }
+}
